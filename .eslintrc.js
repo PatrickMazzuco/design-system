f39b76a7fcf79f2ccdfa065efde75b18a1706c64
PatrickMazzuco <patrickmazzuco@gmail.com>
2022-10-11 20:54:48 -0300

    chore: initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,78 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:tailwindcss/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [
+    "import",
+    "eslint-plugin-import-helpers",
+    "jsx-a11y",
+    "react",
+    "react-hooks",
+    "@typescript-eslint",
+    "tailwindcss",
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/require-default-props": "off",
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".jsx", ".tsx"],
+      },
+    ],
+    "react/jsx-props-no-spreading": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "after-used",
+        argsIgnorePattern: "_",
+        ignoreRestSiblings: false,
+        varsIgnorePattern: "_",
+      },
+    ],
+    "@typescript-eslint/no-namespace": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+        "": "never",
+      },
+    ],
+    "import/no-unresolved": "off",
+    "import/prefer-default-export": "off",
+    "import-helpers/order-imports": [
+      "warn",
+      {
+        newlinesBetween: "always",
+        groups: [["module"], ["/^@//", "parent", "sibling", "index"]],
+        alphabetize: {
+          order: "asc",
+          ignoreCase: true,
+        },
+      },
+    ],
+    "import/order": "off",
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+export function App() {
+  return (
+    <h1 className="w-full bg-zinc-300 text-lg text-zinc-800">React App</h1>
+  );
+}
